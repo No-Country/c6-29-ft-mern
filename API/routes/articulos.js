4ed3a9836351a1f6bf51e8606a27b967d70d5f3e
NorSAUCE <you@example.com>
2022-08-09 11:29:40 -0300

    Creada ruta de articulos

---
+++ API/routes/articulos.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+const articulosController = require('../controllers/articulosController');
+
+
+/* GET users listing. */
+router.post('/', articulosController.create);
+router.get('/', articulosController.getAll);
+router.get('/recientes', articulosController.getRecientes);//trae por fecha
+router.get('/favoritos', articulosController.favoritos);//trae array de favoritos del usuario
+router.get('/localizacion',articulosController.localizar);//trae por coordenadas ubi aprox
+router.get('/articulosuser',articulosController.articulosuser);//trae los articulos que publico el usuario 
+router.get('/:id',  articulosController.getOne);
+router.put('/:id', articulosController.update);
+router.delete('/:id',  articulosController.delete);
+
+module.exports = router;
