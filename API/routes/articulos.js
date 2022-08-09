0bad5d01583c4c2602be88a10fc5660a220f22aa
NorSAUCE <you@example.com>
2022-08-09 14:00:23 -0300

    se creo la ruta para busqueda por nombre

--- API/routes/articulos.js
+++ API/routes/articulos.js
@@ -1,11 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const router = require("express").Router();
 const articulosController = require('../controllers/articulosController');
 
 
 /* GET users listing. */
 router.post('/', articulosController.create);
+router.get('/buscarnombre',articulosController.getnombre);
 router.get('/', articulosController.getAll);
+
 router.get('/recientes', articulosController.getRecientes);//trae por fecha
 router.get('/favoritos', articulosController.favoritos);//trae array de favoritos del usuario
 router.get('/localizacion',articulosController.localizar);//trae por coordenadas ubi aprox
