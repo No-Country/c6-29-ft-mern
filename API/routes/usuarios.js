29b3ca24c343ab7b03656208f55c0cc754ad3c3b
Facundo Serna <30941820+FacundoSerna96@users.noreply.github.com>
2022-08-09 15:50:17 -0300

    Se agregan rutas nuevas al usuario
    buscar por nombre
    favoritos

--- API/routes/usuarios.js
+++ API/routes/usuarios.js
@@ -2,9 +2,14 @@ const router = require("express").Router();
 const usersController = require('../controllers/usersController')
 
 
-router.get('/',usersController.get);
+
+router.get('/buscanombre', usersController.buscaNombre);
+router.get('/favoritos', usersController.favoritos);
 router.get('/articulosuser',usersController.articulosuser);//trae los articulos que publico el usuario 
+
+router.get('/',usersController.get);
 router.get('/:id',usersController.getOne);
+
 router.post('/',usersController.create);
 router.put('/:id',usersController.update);
 router.delete('/:id',usersController.delete);
