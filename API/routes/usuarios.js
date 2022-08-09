c40082f815246438762f19dc10b868fb21ce6dbb
NorSAUCE <you@example.com>
2022-08-09 14:45:45 -0300

    cambio en la linea 6 por usersController

--- API/routes/usuarios.js
+++ API/routes/usuarios.js
@@ -3,7 +3,7 @@ const usersController = require('../controllers/usersController')
 
 
 router.get('/',usersController.get);
-router.get('/articulosuser',articulosController.articulosuser);//trae los articulos que publico el usuario 
+router.get('/articulosuser',usersController.articulosuser);//trae los articulos que publico el usuario 
 router.get('/:id',usersController.getOne);
 router.post('/',usersController.create);
 router.put('/:id',usersController.update);
