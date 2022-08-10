fd255d5edb6274ec98e5e87fa8756a2a9a1e3978
NorSAUCE <you@example.com>
2022-08-10 17:08:10 -0300

    borrado de la linea 34

--- API/database/config.js
+++ API/database/config.js
@@ -1,18 +1,21 @@
 
 const mongoose = require('mongoose');
-
+const mongoosePaginate= require('mongoose-paginate-v2');
 const dbConnection = async() => {
-
 mongoose.connect(process.env.MONGODB_CNN,function(error){
+    
     if(error){
         throw error;
     }else{
-        console.log("Conectado con mongodb");
+           console.log("Conectado con mongodb");
     }
 });
 
+   
 }
 
+
 module.exports ={
+    
     dbConnection
 }
