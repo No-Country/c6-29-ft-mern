a9d320d3ebef844e9fbe8c743b07989366c6d645
NorSAUCE <you@example.com>
2022-08-10 15:03:39 -0300

    configurando paginate

--- API/database/config.js
+++ API/database/config.js
@@ -1,16 +1,18 @@
 
 const mongoose = require('mongoose');
-
+const mongoosePaginate=require('mongoose-paginate-v2');
 const dbConnection = async() => {
-
-mongoose.connect(process.env.MONGODB_CNN,function(error){
+   mongoose.connect(process.env.MONGODB_CNN,function(error){
     if(error){
         throw error;
     }else{
         console.log("Conectado con mongodb");
     }
 });
-
+mongoosePaginate.paginate.options={
+    limit:10
+}
+mongoose.mongoosePaginate=mongoosePaginate
 }
 
 module.exports ={
