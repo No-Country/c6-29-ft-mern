fe8c24b71ea1e098053d34d1599f145480f1befe
NorSAUCE <you@example.com>
2022-08-11 14:04:30 -0300

    se agrega la ruta /categorias al servidor

--- API/server.js
+++ API/server.js
@@ -6,6 +6,7 @@ const port = 3000
 const { dbConnection } = require('./database/config');
 var usersRouter = require('./routes/usuarios');
 var articulosRouter =require('./routes/articulos');
+var categoriasRouter =require('./routes/categorias');
 const database = async () => {
     await dbConnection();
 }
@@ -21,6 +22,7 @@ app.use(cors());
 app.use(express.json())
 app.use('/usuarios', usersRouter);
 app.use('/articulos',articulosRouter);
+app.use('/categorias',categoriasRouter);
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
