68496b8d020cea9caeda7faecbea63714b8f3842
Alan Naranjo <85073731+L-A-M-J-O@users.noreply.github.com>
2022-08-11 23:33:44 +0200

    Buscador Terminado
    Falta Funcionalidad los estilos ya quedaron!

--- client/src/views/home/component/Buscador.jsx
+++ client/src/views/home/component/Buscador.jsx
@@ -7,9 +7,9 @@ function Buscador ( ) {
         <div className="container">
             <div className="row justify-content-center mt-5">
                 <div className="col-9 col-md-11 col-lg-11">
-                    <div class="input-group mb-3 rounded-pill border border-2">
-                        <span class="" id="basic-addon1"> <button class="btn btn-Buscador" type="button" id="button-addon1"><img src= {IconBuscador} alt="Icon-Buscador"/></button></span>
-                        <input type="text" class="form-control text-start border border-0" placeholder="Buscar" aria-describedby="button-addon1" aria-label="Buscador"/>
+                    <div className="input-group mb-3 rounded-pill border border-2">
+                        <span className="" id="basic-addon1"> <button className="btn btn-Buscador" type="button" id="button-addon1"><img src= {IconBuscador} alt="Icon-Buscador"/></button></span>
+                        <input type="text" className="form-control text-start border border-0 rounded-pill" placeholder="Buscar" aria-describedby="button-addon1" aria-label="Buscador"/>
                     </div>
                 </div>
                 <div className="col-2 col-md-1 col-lg-1">
