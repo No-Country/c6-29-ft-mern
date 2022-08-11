e62894c681e24c9dee267ed969e0ad7e568cba42
Lautaro <lautaroamarchetti@gmail.com>
2022-08-11 17:19:25 -0300

    generando ProfileMenu y transiciones

--- client/src/views/header/Header.jsx
+++ client/src/views/header/Header.jsx
@@ -6,32 +6,72 @@ import "../../Styles/header.css"
 
 const Header = () => {
 
+
+    /// FUNCION QUE MUESTRA Y ANIMA EL MENU HAMBURGUESA
+
     const showNavMenu = () => {
-        const navMenu = document.querySelector("[data-nav-menu]")
+        const navMenu = document.querySelector("[data-nav-menu]");
         const bars = document.querySelector("[data-bars]");
-        navMenu.classList.toggle("visible")
+        
+        if(navMenu.classList.contains("visible")){
+            
+            navMenu.style.opacity = 0
+            setTimeout(() => {
+                navMenu.classList.remove("visible");
+            },400)
+        } else {
+            navMenu.classList.add("visible")
+            setTimeout(() => {
+                navMenu.style.opacity = 1
+            },100)
+        }
+        
         }
 
+    /// FUNCION QUE MUESTRA Y ANIMA EL MENU PROFILE
 
     const showProfileMenu = () =>{
+        const profileMenu = document.querySelector("[data-profile-menu]");
 
-    }
+        if(profileMenu.classList.contains("visible")){
+            
+            profileMenu.style.opacity = 0
+            setTimeout(() => {
+                profileMenu.classList.remove("visible");
+            },1000)
+        } else {
+            profileMenu.classList.add("visible")
+            setTimeout(() => {
+                profileMenu.style.opacity = 1
+            },100)
+        }
+        
+        }
 
     return(
         <div className="header-container" data-header-container>
             <img src={Bars} onClick={showNavMenu} data-bars/>
-            <img src={GsLogo}/>
-            <img src={UserLogo} data-profile/>
-    
-            <div class="nav-menu__container" data-nav-menu>
-                <ul class="container__list">
-                    <li class="list__items"><Link to='/login'>login</Link></li>
-                    <li class="list__items">Vender</li>
-                    <li class="list__items">Donar</li>
-                    <li class="list__items">Contacto</li>
+            <Link to='/'><img src={GsLogo}/></Link>
+            <img src={UserLogo} onClick={showProfileMenu} data-profile/>
+            
+            <div className="nav-menu__container" data-nav-menu>
+                <ul className="container__list">
+                    <li className="list__items"><Link to='/login'>Inicio</Link></li>
+                    <li className="list__items"><Link to='/'>Vender</Link></li>
+                    <li className="list__items"><Link to='/'>Donar</Link></li>
+                    <li className="list__items"><Link to='/'>Contacto</Link></li>
                 </ul>
             </div>
-        </div>
+            
+            <div className="nav-menu__container" data-profile-menu>
+                <ul className="container__list">
+                    <li className="list__items"><Link to='/login'>Iniciar Sesión</Link></li>
+                    <li className="list__items"><Link to= '/register'>Registrarse</Link></li>
+                    <li className="list__items"><Link to='/'>Donar</Link></li>
+                    <li className="list__items"><Link to='/'>Contacto</Link></li>
+                </ul>
+            </div>
+        </div>        
     )
 }
 
