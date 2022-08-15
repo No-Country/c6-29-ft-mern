36c4e685a8e72fedba7804c56ffc9e66ef251593
Alan Naranjo <85073731+L-A-M-J-O@users.noreply.github.com>
2022-08-15 23:18:03 +0200

    Arregles las Card
    volvi a crear las card de nuevo, me falta a finar detalles

--- client/src/views/home/component/Novedades.jsx
+++ client/src/views/home/component/Novedades.jsx
@@ -12,70 +12,60 @@ function Novedades ( ) {
                 </div>
                 <div className="col-3">
                     <Link className="text-ver text-center" to='/vermas'>Ver más</Link>
-                </div>  
-                <div className="col-6">
-                    <div className="card-main card mb-3">
-                        <div className="row g-0">
-                                <div className="col-md-2">
-                                    <img src="https://lp2.hm.com/hmgoepprod?set=source[/99/38/99380ebf278be1c548eddc9f5a0a3894a6acc91c.jpg],origin[dam],category[],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]" className="img-fluid img-card" alt="producto"/>
-                                </div>
-                                <div className="col-md-8">
-                                    <div className="card-body">
-                                        <h6 className="card-title">Nombre del producto</h6>
-                                        <p className="card-text "><img src={ Pin } alt="pin"/><small className="ubication">Ubication</small></p>    
-                                        <p className="price card-text">Price$</p>
-                                    </div>
+                </div> 
+            </div>
+            <div className="row main__card justify-content-between">
+                <div className="col-6 mb-3 box__card bg-white rounded-2">
+                                <div className="row row-cols-1">
+                                    <div className="col-5 card-main">
+                                    <img src="https://lp2.hm.com/hmgoepprod?set=source[/99/38/99380ebf278be1c548eddc9f5a0a3894a6acc91c.jpg],origin[dam],category[],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]" className="img-card" alt="producto"/>
+                            </div>
+                            <div className="col-7 ms-0">
+
+                                    <h6 className="card-title">Nombre del producto</h6>
+                                    <p className="card-text "><img src={ Pin } alt="pin"/><small className="ubication">Ubication</small></p>    
+                                    <p className="price card-text">Price$</p>
                             </div>
                         </div>
-                    </div>
                 </div>
-                <div className="col-6">
-                    <div className="card-main card mb-3">
-                        <div className="row g-0">
-                                <div className="col-md-2">
-                                    <img src="https://lp2.hm.com/hmgoepprod?set=source[/99/38/99380ebf278be1c548eddc9f5a0a3894a6acc91c.jpg],origin[dam],category[],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]" className="img-fluid img-card" alt="producto"/>
-                                </div>
-                                <div className="col-md-8">
-                                    <div className="card-body">
-                                        <h6 className="card-title">Nombre del producto</h6>
-                                        <p className="card-text "><img src={ Pin } alt="pin"/><small className="ubication">Ubication</small></p>    
-                                        <p className="price card-text">Price$</p>
-                                    </div>
+                <div className="col-6 mb-3 box__card bg-white rounded-2">
+                                <div className="row row-cols-1">
+                                    <div className="col-5 card-main">
+                                    <img src="https://lp2.hm.com/hmgoepprod?set=source[/99/38/99380ebf278be1c548eddc9f5a0a3894a6acc91c.jpg],origin[dam],category[],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]" className="img-card" alt="producto"/>
+                            </div>
+                            <div className="col-7 ms-0">
+
+                                    <h6 className="card-title">Nombre del producto</h6>
+                                    <p className="card-text "><img src={ Pin } alt="pin"/><small className="ubication">Ubication</small></p>    
+                                    <p className="price card-text">Price$</p>
                             </div>
                         </div>
-                    </div>
                 </div>
-                <div className="col-6">
-                    <div className="card-main card mb-3">
-                        <div className="row g-0">
-                                <div className="col-md-2">
-                                    <img src="https://lp2.hm.com/hmgoepprod?set=source[/99/38/99380ebf278be1c548eddc9f5a0a3894a6acc91c.jpg],origin[dam],category[],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]" className="img-fluid img-card" alt="producto"/>
-                                </div>
-                                <div className="col-md-8">
-                                    <div className="card-body">
-                                        <h6 className="card-title">Nombre del producto</h6>
-                                        <p className="card-text "><img src={ Pin } alt="pin"/><small className="ubication">Ubication</small></p>    
-                                        <p className="price card-text">Price$</p>
-                                    </div>
+                <div className="col-6 mb-3 box__card bg-white rounded-2">
+                                <div className="row row-cols-1">
+                                    <div className="col-5 card-main">
+                                    <img src="https://lp2.hm.com/hmgoepprod?set=source[/99/38/99380ebf278be1c548eddc9f5a0a3894a6acc91c.jpg],origin[dam],category[],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]" className="img-card" alt="producto"/>
+                            </div>
+                            <div className="col-7 ms-0">
+
+                                    <h6 className="card-title">Nombre del producto</h6>
+                                    <p className="card-text "><img src={ Pin } alt="pin"/><small className="ubication">Ubication</small></p>    
+                                    <p className="price card-text">Price$</p>
                             </div>
                         </div>
-                    </div>
                 </div>
-                <div className="col-6">
-                    <div className="card-main card mb-3">
-                        <div className="row g-0">
-                                <div className="col-md-2">
-                                    <img src="https://lp2.hm.com/hmgoepprod?set=source[/99/38/99380ebf278be1c548eddc9f5a0a3894a6acc91c.jpg],origin[dam],category[],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]" className="img-fluid img-card" alt="producto"/>
-                                </div>
-                                <div className="col-md-8">
-                                    <div className="card-body">
-                                        <h6 className="card-title">Nombre del producto</h6>
-                                        <p className="card-text "><img src={ Pin } alt="pin"/><small className="ubication">Ubication</small></p>    
-                                        <p className="price card-text">Price$</p>
-                                    </div>
+                <div className="col-6 mb-3 box__card bg-white rounded-2">
+                                <div className="row row-cols-1">
+                                    <div className="col-5 card-main">
+                                    <img src="https://lp2.hm.com/hmgoepprod?set=source[/99/38/99380ebf278be1c548eddc9f5a0a3894a6acc91c.jpg],origin[dam],category[],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]" className="img-card" alt="producto"/>
+                            </div>
+                            <div className="col-7 ms-0">
+
+                                    <h6 className="card-title">Nombre del producto</h6>
+                                    <p className="card-text "><img src={ Pin } alt="pin"/><small className="ubication">Ubication</small></p>    
+                                    <p className="price card-text">Price$</p>
                             </div>
                         </div>
-                    </div>
                 </div>
             </div>
         </div>
