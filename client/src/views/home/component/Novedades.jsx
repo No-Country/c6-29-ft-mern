ee0d082ee039041944e4c37999bb1e8e10c62371
Alan Naranjo <85073731+L-A-M-J-O@users.noreply.github.com>
2022-08-17 00:04:36 +0200

    Pruebas de Card
    Agregue una tarjeta manuelmente!

--- client/src/views/home/component/Novedades.jsx
+++ client/src/views/home/component/Novedades.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import {Link} from 'react-router-dom';
 import Pin from '../../../img/Pin.png';
+import Prueba from '../../../img/product.png'
+
 import '../../../Styles/Novedades.css';
 
 function Novedades ( ) {
@@ -18,13 +20,13 @@ function Novedades ( ) {
                 <div className="col-6 mb-3 box__card bg-white rounded-2">
                                 <div className="row row-cols-1">
                                     <div className="col-5 card-main">
-                                    <img src="https://lp2.hm.com/hmgoepprod?set=source[/99/38/99380ebf278be1c548eddc9f5a0a3894a6acc91c.jpg],origin[dam],category[],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]" className="img-card" alt="producto"/>
+                                    <img src= {Prueba} className="img-card" alt="producto"/>
                             </div>
                             <div className="col-7 ms-0">
 
-                                    <h6 className="card-title">Nombre del producto</h6>
-                                    <p className="card-text "><img src={ Pin } alt="pin"/><small className="ubication">Ubication</small></p>    
-                                    <p className="price card-text">Price$</p>
+                                    <h6 className="card-title">Iphone 7 32GB</h6>
+                                    <p className="card-text "><img src={ Pin } alt="pin"/><small className="ubication">Belgrano</small></p>    
+                                    <p className="price card-text">$94.390</p>
                             </div>
                         </div>
                 </div>
