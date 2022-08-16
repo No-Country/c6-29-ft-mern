146c1992b32e79d6d44174513586c0f46b68ef4c
NorSAUCE <you@example.com>
2022-08-16 13:33:17 -0300

    Modificando expresion lines 6o y 61

--- API/config/validator.js
+++ API/config/validator.js
@@ -1,4 +1,4 @@
-const joi = require('joi')
+const Joi = require('joi')
 
 const Joi = require('@hapi/joi')
 Joi.objectId = require('joi-objectid')(Joi)
@@ -6,98 +6,98 @@ Joi.objectId = require('joi-objectid')(Joi)
 
 const validator = (req, res, next)=>{
 
-    const schema = joi.object({
+    const schema = Joi.object({
         
-        username: joi.string().max(20).min(2).trim().pattern(new RegExp('[a-zA-Z]')).required().messages({
+        username: Joi.string().max(20).min(2).trim().pattern(new RegExp('[a-zA-Z]')).required().messages({
             'string.min':'You name must have at least 2 characters',
             'string.max':'You name cannot exceed 20 characters'
         }),
-        password: joi.string().pattern(new RegExp('[a-zA-Z0-9]')).required().trim().min(8).max(30).message({
+        password: Joi.string().pattern(new RegExp('[a-zA-Z0-9]')).required().trim().min(8).max(30).message({
             'string.min': 'The password must be have a minimum length of 8 characters (lower case, upper case and numberes are required).',
             'string.pattern': 'The pasword must be alphanumerical and it must contain at least one number.'
         }),
-        email: joi.string().email({minDomainSegments: 2}).required().messages({
+        email: Joi.string().email({minDomainSegments: 2}).required().messages({
             'string.email':'Incorrect email format.'
         }),
-        emailValidad: joi.boolean(),
-        google: joi.boolean(),
-        rol: joi.string(),
-        avatar: joi.string().min(5).trim().messages({
+        emailValidad: Joi.boolean(),
+        google: Joi.boolean(),
+        rol: Joi.string(),
+        avatar: Joi.string().min(5).trim().messages({
             'string.min':'URL must have at least 5 characters',
         }),
 
         contacto:{
-            nombre: joi.string().max(20).min(2).trim().pattern(new RegExp('[a-zA-Z]')).messages({
+            nombre: Joi.string().max(20).min(2).trim().pattern(new RegExp('[a-zA-Z]')).messages({
                 'string.min':'You name must have at least 2 characters',
                 'string.max':'You name cannot exceed 20 characters',
                 'string.pattern': ''
             }),
-            apellido: joi.string().max(20).min(2).trim().pattern(new RegExp('[a-zA-Z]')).messages({
+            apellido: Joi.string().max(20).min(2).trim().pattern(new RegExp('[a-zA-Z]')).messages({
                 'string.min':'You name must have at least 2 characters',
                 'string.max':'You name cannot exceed 20 characters',
                 'string.pattern': ''
             }), 
-            telefono:joi.string(),
-            email: joi.string(),
-            direccion: joi.string(),
+            telefono:Joi.string(),
+            email: Joi.string(),
+            direccion: Joi.string(),
             coordenadas:{
-                latitud: joi.number(),
-                longitud: joi.number(),
+                latitud: Joi.number(),
+                longitud: Joi.number(),
             }
         },
 
 
         feria:{
-            nombre:joi.string(),
-            ubicacionReal:joi.string(),
+            nombre:Joi.string(),
+            ubicacionReal:Joi.string(),
             coordenadas:{
-                latitud: joi.number(),
-                longitud: joi.number(),
+                latitud: Joi.number(),
+                longitud: Joi.number(),
             }
         },
 
 
-        articulos_id : joi.array.items(Joi.objectId()),
-        favoritos_id : joi.array.items(joi.objectId()),
+        articulos_id : Joi.array.items('Joi-objectId')(Joi),
+        favoritos_id : Joi.array.items('Joi-objectId')(Joi),
 
     })
 
-    const schemaArticulo = joi.object({ 
-        nombreArticulo: joi.string().max(20).min(2).trim().pattern(new RegExp('[a-zA-Z]')).required().messages({
+    const schemaArticulo = Joi.object({ 
+        nombreArticulo: Joi.string().max(20).min(2).trim().pattern(new RegExp('[a-zA-Z]')).required().messages({
             'string.min':'You name must have at least 2 characters',
             'string.max':'You name cannot exceed 20 characters'
         }), 
 
-        fechaCreacion: joi.date(),
-        imagen: joi.array.items(joi.string()),
-        descripcion: joi.string().max(150).trim(),
-        categoria: joi.string().required(),
-        condicion: joi.string().required(),
-        precio: joi.string().required(),
+        fechaCreacion: Joi.date(),
+        imagen: Joi.array.items(Joi.string()),
+        descripcion: Joi.string().max(150).trim(),
+        categoria: Joi.string().required(),
+        condicion: Joi.string().required(),
+        precio: Joi.string().required(),
         ubicacion:{
-            latitud: joi.number(),
-            longitud: joi.number()
+            latitud: Joi.number(),
+            longitud: Joi.number()
         },
-        estado: joi.string().required(),
-        valoracion: joi.number(),
+        estado: Joi.string().required(),
+        valoracion: Joi.number(),
 
         contacto:{
-            nombre: joi.string().max(20).min(2).trim().pattern(new RegExp('[a-zA-Z]')).messages({
+            nombre: Joi.string().max(20).min(2).trim().pattern(new RegExp('[a-zA-Z]')).messages({
                 'string.min':'You name must have at least 2 characters',
                 'string.max':'You name cannot exceed 20 characters',
                 'string.pattern': ''
             }),
-            apellido: joi.string().max(20).min(2).trim().pattern(new RegExp('[a-zA-Z]')).messages({
+            apellido: Joi.string().max(20).min(2).trim().pattern(new RegExp('[a-zA-Z]')).messages({
                 'string.min':'You name must have at least 2 characters',
                 'string.max':'You name cannot exceed 20 characters',
                 'string.pattern': ''
             }), 
-            telefono:joi.string(),
-            email: joi.string(),
-            direccion: joi.string(),
+            telefono:Joi.string(),
+            email: Joi.string(),
+            direccion: Joi.string(),
             coordenadas:{
-                latitud: joi.number(),
-                longitud: joi.number(),
+                latitud: Joi.number(),
+                longitud: Joi.number(),
             }
         },
 
