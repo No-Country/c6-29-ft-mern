b679c7a24d13afeec54732f59e38fb9e8b14a018
NorSAUCE <you@example.com>
2022-08-17 10:34:25 -0300

    cambiando puerto al 3001 Linea 5

--- API/server.js
+++ API/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 require('dotenv').config()
 const cors = require('cors')
 const app = express()
-const port = 3000
+const port = 3001
 const { dbConnection } = require('./database/config');
 
 var usersRouter = require('./routes/usuarios');
