ca3781a847786182741b612626a5cfc91ceba13c
NorSAUCE <you@example.com>
2022-08-18 15:30:55 -0300

    quite populate

--- API/controllers/articulosController.js
+++ API/controllers/articulosController.js
@@ -6,7 +6,7 @@ module.exports ={
           const query = {};
 
           const options = {
-            populate: ['categoria', 'contacto'],
+            populate: ['categoria'],
             limit: 10,
           }
           const articulosAll = await articulos.paginate(query, options)
