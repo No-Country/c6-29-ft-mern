91c3ff3708580b3a2c20527a1c8a791c5f6ff76b
Lautaro <lautaroamarchetti@gmail.com>
2022-08-18 11:35:37 -0300

    agregando estados y vinculacion API

--- client/src/views/register/Register.jsx
+++ client/src/views/register/Register.jsx
@@ -2,23 +2,40 @@ import Separador from "../../img/Separador.svg"
 import FacebookLogo from "../../img/facebook-logo.png"
 import GoogleLogo from "../../img/google-logo.png"
 import GsLogo from "../../img/gs-logo.png"
-
+import { useState } from "react";
 
 const Register = () => {
 
+const [name,setName] = useState ("");
+const [email,setEmail] = useState ("");
+const [pass,setPass] = useState ("");
+const [doublePass,setDoublePass] = useState ("");
+
 const form = document.querySelector("[data-register-form]");
 
-    const createUser = () => {
-        fetch("http://localhost:3001/usuarios/%22,%7B",{
+    const createUser = (e) => {
+       e.preventDefault();
+       if(pass !== doublePass){
+        console.log("contraseñas no coinciden");
+       } else {
+        const object = {
+            nombre: name,
+            mail: email,
+            pass: pass,
+        };    
+           console.log(object)
+    
+           fetch("http://localhost:3001/usuarios/%22,%7B",{
+    
+                method:'POST',
+                headers:{
+                    'content-type':'application/json'
+                },
+                body:JSON.stringify({form: object})
+            })
+            }
+       }     
 
-            method:'POST',
-            headers:{
-                'content-type':'application/json'
-            },
-            body:JSON.stringify(form)
-        })
-    }
-       
 
     return(
         <div className="login__container">
@@ -26,11 +43,11 @@ const form = document.querySelector("[data-register-form]");
                 <img className="logo__item" src={GsLogo}/>
             </div>
             <h2 className="container__title">Creá tu <span>cuenta</span></h2>
-            <form className="input__container" data-register-form>
-                <input type="email" className="input-container__item" id="email-input" placeholder="Correo electrónico" required/>
-                <input type="text" className="input-container__item" id="user-input" placeholder="Nombre de usuario" required/>
-                <input type="password" className="input-container__item" id="pass-input" placeholder="Contraseña" required/>
-                <input type="password" className="input-container__item" id="second-pass-input" placeholder="Repetir contraseña" required/>
+            <form className="input__container" data-register-form onSubmit={createUser}>
+                <input type="email" className="input-container__item"  placeholder="Correo electrónico" value={email} onChange={(e)=> setEmail(e.target.value)} required/>
+                <input type="text" className="input-container__item" id="user-input" placeholder="Nombre de usuario" value={name}  onChange={(e)=> setName(e.target.value)}required/>
+                <input type="password" className="input-container__item" id="pass-input" placeholder="Contraseña" value={pass}  onChange={(e)=> setPass(e.target.value)} required/>
+                <input type="password" className="input-container__item" id="second-pass-input" placeholder="Repetir contraseña" value={doublePass}  onChange={(e)=> setDoublePass(e.target.value)} required/>
                 <button className="input-container__button">Crear cuenta</button>
             </form>
             <img src={Separador} alt="" className="login__separador"/>
