fa416b64904df8c1398a5f648f88298407cccec5
Lautaro <lautaroamarchetti@gmail.com>
2022-08-18 22:25:07 -0300

    creando mensaje registro exitoso

--- client/src/views/register/Register.jsx
+++ client/src/views/register/Register.jsx
@@ -2,7 +2,9 @@ import Separador from "../../img/Separador.svg"
 import FacebookLogo from "../../img/facebook-logo.png"
 import GoogleLogo from "../../img/google-logo.png"
 import GsLogo from "../../img/gs-logo.png"
+import checkDone from "../../img/check.png"
 import { useState } from "react";
+import {Link} from 'react-router-dom';
 
 const Register = () => {
 
@@ -34,19 +36,28 @@ const [doublePass,setDoublePass] = useState ("");
               body:JSON.stringify(object),
             });
             const res = await response.json();
-        
+            showRegisterMessage();
             return res;
           } catch (error) {
         
             console.log(error);
           }
     }    
+}
   
-
+// FUNCION PARA REDIRECCIONAR AL LOGIN Y MOSTRAR MENSAJE DE ARRIBO CORREO
+    
+const showRegisterMessage = () =>{
+    const message = document.querySelector("[data-message-container]")
+    const cuerpo = document.querySelector("body");
+    message.style.display = "block";
+    message.style.height = `${cuerpo.clientHeight}px`;
+    window.scrollTo(0,0)
+    console.log(message);
 }
 
 return(
-    <div className="login__container">
+    <div className="login__container" data-register-container>
         <div className="login__logo-container">
             <img className="logo__item" src={GsLogo}/>
         </div>
@@ -56,7 +67,7 @@ return(
             <input type="text" className="input-container__item" id="user-input" placeholder="Nombre de usuario" value={name}  onChange={(e)=> setName(e.target.value)}required/>
             <input type="password" className="input-container__item" id="pass-input" placeholder="Contraseña" value={pass}  onChange={(e)=> setPass(e.target.value)} required/>
             <input type="password" className="input-container__item" id="second-pass-input" placeholder="Repetir contraseña" value={doublePass}  onChange={(e)=> setDoublePass(e.target.value)} required/>
-            <button className="input-container__button"onClick={createUser}>Crear cuenta</button>
+            <button className="input-container__button" onClick={createUser}>Crear cuenta</button>
         </form>
         <img src={Separador} alt="" className="login__separador"/>
         <div className="redes__container">
@@ -67,7 +78,18 @@ return(
                 <img src={GoogleLogo} alt="Google"  />
             </div>
         </div>
+    
+        <div className="message-container" data-message-container>
+            <div className="message-container__box">
+                <h4 className="message-container__title">¡Felicitaciones!</h4>
+                <img className="message-container__img" src={checkDone}/>
+                <h5 className="message-container__sub-title">Tu cuenta fue creada con exito.</h5>
+                <p className="message-container__details">Enviamos un mail a tu correo</p>
+                <Link to="/login" ><button className="message-container__button">Iniciar Sesion</button></Link>
+            </div>
+        </div>
     </div>
 )
+
 }
 export default Register
