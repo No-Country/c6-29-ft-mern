28bfa42e649e6462da3453cc2858fb3c751d6c14
NorSAUCE <you@example.com>
2022-08-20 17:49:29 -0300

    implementado el buscador. Falta estilos

--- client/src/views/home/component/Buscador.jsx
+++ client/src/views/home/component/Buscador.jsx
@@ -1,29 +1,49 @@
-import React from "react";
-
-import IconBuscador from '../../../img/IconoBuscar.png';
-import filtro from '../../../img/Botón-filtro.png'
-
+import React, { useEffect, useState } from "react";
+import SecondaryCards from "./SecondaryCards";
 import '../../../Styles/Buscador.css';
 
 function Buscador ( ) {
+    const [data,setData] = useState([]);
+    const [buscar,setBuscar] = useState('')
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+              const response = await fetch(
+                "http://localhost:3001/articulos/buscarnombre/?buscar="+buscar,{
+                  method:'GET',
+                  headers: {
+                    'Content-type': 'application/json',
+                },
+                }
+              );
+      
+              const res= await response.json()
+              setData(res)
+            
+            } catch (err) {
+              console.log(err);
+            }
+          };
+          fetchData()
+       }, [buscar])
+       
+       console.log(data)
+
+
+       const handleBuscar=(e)=>{
+        const value = e.target.value
+        setBuscar(value)
+    }
     return ( 
-        <div className="container-fluid">
-            <div className="row justify-content-center mb-4 mt-4">
-                <div className="col-9 col-md-10 box-input border border-1 rounded-pill me-md-2">
-                    <div className="row">
-                        <div className="col-1 me-2">
-                            <span className="" id="basic-addon1"> <button className="btn btn-Buscador" type="button" id="button-addon1"><img src= {IconBuscador} alt="Icon-Buscador"/></button></span>
-                        </div>
-                        <div className="col-7">
-                            <input type="text" className="form-control text-start border border-0 rounded-pill" placeholder="Buscar" aria-describedby="button-addon1" aria-label="Buscador"/>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-2 col-md-1 ms-xs-1">
-                    <img src= {filtro} alt ="filtre"/>
-                </div>
+        <>
+        <input type="text" value={buscar} onChange={handleBuscar}></input>
+            <div>
+                <row>
+                {data.map((e)=> (<SecondaryCards key={e._id} imgURL={e.imagen[0]} productName={e.nombreArticulo} productLocation={e.ubicacion.longitud + "," + e.ubicacion.latitud} productPrice={"$" + e.precio}/>))}
+                </row>
             </div>
-        </div>
+    </>
     );
 }
 export default Buscador;
