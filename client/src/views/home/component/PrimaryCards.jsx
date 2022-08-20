e9125008bc452e1b9baada54f71824e794cab01e
NorSAUCE <you@example.com>
2022-08-20 16:26:32 -0300

    quitando precio para que no muestre en donaciones

--- client/src/views/home/component/PrimaryCards.jsx
+++ client/src/views/home/component/PrimaryCards.jsx
@@ -1,14 +1,13 @@
 import Pin from '../../../img/Pin.png';
 
-export const PrimaryCards = ({imgURL,productName,productLocation,productPrice}) => {
+export const PrimaryCards = ({imgURL,productName,productLocation}) => {
     return(
         <div className="col-6">
                         <div className="card border border-0 card-main">
                             <img src= {imgURL}  className="card-img-top" alt="product-1"/>
                             <div className="card-body">
                                 <h5 className="card-title card-title-donaciones">{productName}</h5>
-                                <p className="card-text "><img src={ Pin } alt="pin"/><small className="ubication">{productLocation}</small></p>    
-                                <p className="price card-text">{productPrice}</p>
+                                <p className="card-text "><img src={ Pin } alt="pin"/><small className="ubication">{productLocation}</small></p> 
                             </div>
                         </div>
                     </div>
