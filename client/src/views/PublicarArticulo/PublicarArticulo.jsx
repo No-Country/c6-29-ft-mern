46f553d73d68720d88a40f68a7ce37afb8310182
NorSAUCE <you@example.com>
2022-08-25 15:29:31 -0300

    creacion del fetch y los value

--- client/src/views/PublicarArticulo/PublicarArticulo.jsx
+++ client/src/views/PublicarArticulo/PublicarArticulo.jsx
@@ -6,13 +6,70 @@ import Flecha from "../../img/flecha.png";
 import { Link, } from 'react-router-dom';
 import Subirimagen from './Subirimagen';
 import Categorias from './Categorias';
-
+import { useState } from "react";
 const PublicarArticulo = () => {
-    const url = (result) =>{
-        console.log(result);
-    }
+    
+const [usuario,setUsuario] = useState ("");    
+const [nombre,setNombre] = useState ("");
+const [fecha,setFecha] = useState ("");
+const [url,setUrl] = useState ("");
+const  [descripcion,setDescripcion]  = useState ("")
+const [categoria,setCategoria] = useState ("");
+const [precio,setPrecio] = useState ("");
+const [provincia,setProvincia] = useState ("");
+const [localidad,setLocalidad] = useState ("");
+const [alto,setAlto] = useState ("");
+const [ancho,setAncho] = useState ("");
+const [material,setMaterial] = useState ("");
+const [unidades,setUnidades] = useState ("");
 
-    return (
+
+
+    const createArticulo = async (e) => {
+       e.preventDefault();
+ 
+           try {
+            const object = {
+                usuario: usuario,//aca iria id de usuario que traigo con el token._id,
+                nombreArticulo: nombre,
+                fechaCreacion: fecha,//aca tendria que tomar la fecha de la compu con date()
+                imagen:url,
+                descripcion:descripcion,
+                categoria:categoria,
+                precio: precio,
+                ubicacion:{
+                    provincia:provincia,
+                    localidad:localidad
+                },
+                dimension:{
+                    alto:alto,
+                    ancho:ancho
+                },
+                material:material,
+                unidades:unidades,
+            };    
+               console.log(object)
+            const response = await fetch("http://localhost:3001/articulos/", {
+              method: "POST",
+              headers: {
+                "Content-type": "application/json",
+                'x-access-token':localStorage.getItem("token"),//traigo el token
+              },
+                body:JSON.stringify(object),
+            });
+            const res = await response.json();
+            return res;
+          } catch (error) {
+        
+            console.log(error);
+          }
+         }    
+
+   // const url = (result) =>{
+     //   console.log();
+    //}
+
+  return (
         <div className='container'>
             <div className='row justify-content-between box-header m-3'>
                 <div className='col-3'>
@@ -32,7 +89,7 @@ const PublicarArticulo = () => {
                 </div>
                 <div className='col-12 mt-2'>
                     <div className="input-group mb-3">
-                        <input type="text" className="form-control rounded-pill" placeholder="Ingresar título. Ejemplo: Silla de madera" aria-label="Recipient's username" aria-describedby="button-addon2"/>
+                        <input type="text" value={nombre} onChange={(e)=> setNombre(e.target.value)}className="form-control rounded-pill" placeholder="Ingresar título. Ejemplo: Silla de madera" aria-label="Recipient's username" aria-describedby="button-addon2"/>
                     </div>
                 </div>
             </div>
@@ -44,10 +101,10 @@ const PublicarArticulo = () => {
                 <div className="buscador-container ">
                         
 
-                        <input type="text" className="form-control rounded-pill" placeholder="Provincia" aria-label="Recipient's username" aria-describedby="button-addon2"/>
+                        <input type="text" value={provincia} onChange={(e)=> setProvincia(e.target.value)}className="form-control rounded-pill" placeholder="Provincia" aria-label="Recipient's username" aria-describedby="button-addon2"/>
 
 
-                        <input type="text" className="form-control rounded-pill" placeholder="Localidad" aria-label="Recipient's username" aria-describedby="button-addon2"/>
+                        <input type="text" className="form-control rounded-pill" value={localidad} onChange={(e)=> setLocalidad(e.target.value)}placeholder="Localidad" aria-label="Recipient's username" aria-describedby="button-addon2"/>
 
                     </div>
                 </div>
@@ -66,6 +123,7 @@ const PublicarArticulo = () => {
                 </div>
                 <div className='col-12 mt-2'>
                     <div>{<Subirimagen url = {url} />}</div>
+
                 </div>
             </div>
             <div className='row row-cols-1 box-Articule-Imagen mt-4'>
@@ -78,7 +136,7 @@ const PublicarArticulo = () => {
                     </div>
                     <div className='col-4'>
                         <div className="input-group mb-3">
-                            <input type="text" className="form-control rounded-pill" placeholder="Ancho" aria-label="Recipient's username" aria-describedby="button-addon2"/>
+                            <input type="text" className="form-control rounded-pill" value={ancho} onChange={(e)=> setAncho(e.target.value)}placeholder="Ancho" aria-label="Recipient's username" aria-describedby="button-addon2"/>
                         </div>
                     </div>
                     <div className='col-1  align-self-center mb-2'>
@@ -86,7 +144,7 @@ const PublicarArticulo = () => {
                     </div>
                     <div className='col-4'>
                         <div className="input-group mb-3">
-                            <input type="text" className="form-control rounded-pill" placeholder="Alto" aria-label="Recipient's username" aria-describedby="button-addon2"/>
+                            <input type="text" className="form-control rounded-pill" value={alto} onChange={(e)=> setAlto(e.target.value)}placeholder="Alto" aria-label="Recipient's username" aria-describedby="button-addon2"/>
                         </div>
                     </div>
                 </div>
@@ -96,7 +154,7 @@ const PublicarArticulo = () => {
                     </div>
                     <div className='col-9'>
                         <div className="input-group mb-3">
-                            <input type="text" className="form-control rounded-pill" placeholder="Ingresar material" aria-label="Recipient's username" aria-describedby="button-addon2"/>
+                            <input type="text" className="form-control rounded-pill" value={material} onChange={(e)=> setMaterial(e.target.value)}placeholder="Ingresar material" aria-label="Recipient's username" aria-describedby="button-addon2"/>
                         </div>
                     </div>
                 </div>
@@ -106,7 +164,7 @@ const PublicarArticulo = () => {
                     </div>
                     <div className='col-5'>
                         <div className="input-group mb-3">
-                            <input type="text" className="form-control rounded-pill" placeholder="Unidades" aria-label="Recipient's username" aria-describedby="button-addon2"/>
+                            <input type="text" className="form-control rounded-pill" value={unidades} onChange={(e)=> setUnidades(e.target.value)}placeholder="Unidades" aria-label="Recipient's username" aria-describedby="button-addon2"/>
                         </div>
                     </div>
                 </div>
@@ -116,7 +174,7 @@ const PublicarArticulo = () => {
                     <h3>Descripcion</h3>
                 </div>
                 <div className='col-12'>
-                    <input type="text" className="form-control description" placeholder="Ingresar descripción del producto." aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg"/>
+                    <input type="text" className="form-control description" value={descripcion} onChange={(e)=> setDescripcion(e.target.value)}placeholder="Ingresar descripción del producto." aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg"/>
                 </div>
             </div>
             <div className='row row-cols-1 box-Articule-categoria'>
@@ -140,12 +198,12 @@ const PublicarArticulo = () => {
                     <h3>Precio</h3>
                 </div>
                 <div className='col-12'>
-                    <input type="number" className="form-control rounded-pill" placeholder="0" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg"/>
+                    <input type="number" value={precio} onChange={(e)=> setPrecio(e.target.value)} className="form-control rounded-pill" placeholder="0" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg"/>
                 </div>
             </div>
             <div className='row row-cols-1 mt-2  justify-content-center box-Articule-button'>
                 <div className='col-12 m-3'>
-                    <button className='btn  rounded-pill btn-publicar'>Publicar</button>
+                    <button className='btn  rounded-pill btn-publicar'onClick={createArticulo()}>Publicar</button>
                 </div>
             </div>
         </div>
