1ac844424fa71535f02828a5aec0a17bc825a845
Alan Naranjo <85073731+L-A-M-J-O@users.noreply.github.com>
2022-08-31 11:47:10 +0200

    Cambios, de formulario a Desktop

--- client/src/views/PublicarArticulo/Subirimagen.jsx
+++ client/src/views/PublicarArticulo/Subirimagen.jsx
@@ -1,11 +1,33 @@
 import {uploadFile} from '../firebase/config'
 import {useState} from 'react'
 import { render } from '@testing-library/react'
+import { Swal } from 'sweetalert2'
 import "../../Styles/SubirImagen.css"
 
 const Subirimagen = ({getImgUrl}) => {
 
 const [file,setFile] = useState(null)
+
+const Upload = ( ) => {
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top',
+        showConfirmButton: false,
+        timer: 2000,
+        background: '#f6f6f6',
+        timerProgressBar: true,
+        
+        didOpen: (toast) => {
+          toast.addEventListener('mouseenter', Swal.stopTimer)
+          toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+      })
+      
+      Toast.fire({
+        icon: 'success',
+        title: `Se Agrego con exito tu imagen`
+      })
+}
    
 const handleSubmit = async (e) =>{
     e.preventDefault()
@@ -18,16 +40,20 @@ const handleSubmit = async (e) =>{
     } catch(error){
         console.log(error);
     }
-
    }
   
     return(
-    <form onSubmit ={handleSubmit}>
-     <input type="file"
-      name= "" id = ""
-       onChange={(e)=> setFile(e.target.files[0])} data-img-input/>
-       <button>Upload</button>
-    </form>
+        <div className='row justify-content-start'>
+            <div className='col-4 mr-2'>
+                <form className='box-form' onSubmit ={handleSubmit}>
+                    <input className='box-input' type="file" name= " "  id = " " onChange={(e)=> setFile(e.target.files[0])} data-img-input/>
+                </form>
+            </div>
+            <div className='col-4 align-self-center m-0 p-0'>
+            <button className='btn btn-Upload' onClick={Upload}>Upload</button>
+            </div>
+        </div>
+
 )
 }
 export default Subirimagen
